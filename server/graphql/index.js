870724b5630e3763ddff960e97b9756ee5c4c27c
putrasatr <satriaputrapratama2002@gmail.com>
2021-08-09 18:55:21 +0700

    modigy mutation

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -3,8 +3,6 @@ var queryType = require('./queries/');
 var mutations = require('./mutations/index');
 const firebase = require("firebase");
 
-// console.log(mutations)
-
 exports.phonebookSchema = new GraphQLSchema({
     query: queryType,
     mutation: new GraphQLObjectType({
