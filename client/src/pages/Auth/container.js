1914871662deeba3ec5d3aa53bb9826dd12616c1
putrasatr <satriaputrapratama2002@gmail.com>
2021-11-15 22:23:38 +0700

    accesoris

--- client/src/pages/Auth/container.js
+++ client/src/pages/Auth/container.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Box, Text, Stack, Image } from "@chakra-ui/react";
 import BannerImage from "global/img/backdrop.jpeg";
-import Login from "./Login";
 import Register from "./Register";
 
 const Auth = () => {
   return (
     <Stack flex="1" p="10px">
-      <Box w="80%" h="700px" marginLeft="auto" mr="30px" mt="50px">
+      <Box
+        w="80%"
+        h="700px"
+        marginLeft="auto"
+        mr="30px"
+        mt="50px"
+        position="relative"
+      >
         <Image
           src={BannerImage}
           alt=""
@@ -15,8 +21,18 @@ const Auth = () => {
           borderRadius="20px"
           boxShadow="rgb(38, 57, 77) 0px 20px 30px -10px"
         />
+        <Box
+          w="100px"
+          h="100px"
+          bg="primary.main"
+          position="absolute"
+          bottom="12px"
+          right="-12px"
+          border="2px solid green"
+          borderRadius="10px"
+        ></Box>
       </Box>
-      <Box position="absolute" top="100px" left="100px" w="500px">
+      <Box position="absolute" top="120px" left="120px" w="500px">
         <Text fontSize="6pc" color="red">
           Explore yourself
         </Text>
