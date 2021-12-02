fd94e51bb66b2b5a5cb209264e7836b0460957bf
putrasatr <satriaputrapratama2002@gmail.com>
2021-12-02 13:59:36 +0700

    Test unit

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
+const App = () => {
+  return <div>learn react</div>;
+};
+const Welcome = ({ firstName, lastName }) => {
+  return (
+    <div>
+      <div role="form">
+        <input name={"firstName"} value={firstName} />
+        <input name={"lastName"} value={lastName} />
+      </div>
+    </div>
+  );
+};
+
+test("has correct input value", () => {
+  render(<Welcome firstName="John" lastName="Doe" />);
+  expect(screen.getByRole("form")).toHaveFormValues({
+    firstName: "John",
+    lastName: "Doe",
+  });
+});
+
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
