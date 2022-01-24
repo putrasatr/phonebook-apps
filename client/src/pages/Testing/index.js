6ff7ac3f4cbd41009bc387c8b29093c3f86bc12b
putrasatr <satriaputrapratama2002@gmail.com>
2022-01-24 17:10:48 +0700

    Testing react hook form

--- client/src/pages/Testing/index.js
+++ client/src/pages/Testing/index.js
@@ -1,39 +1,93 @@
-import React, { Component } from "react";
+import React from "react";
+import { Stack, Text } from "@chakra-ui/react";
+import AsyncForm from "./Async";
+import Reguler from "./reguler";
 
-export default class YourComponent extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      copySuccess: false,
-    };
-  }
-
-  copyCodeToClipboard = () => {
-    const el = this.textArea;
-    el.select();
-    document.execCommand("copy");
-    this.setState({ copySuccess: true });
-  };
-
-  render() {
-    return (
-      <div>
-        <div>
-          <textarea
-            ref={(textarea) => (this.textArea = textarea)}
-            value="Example copy for the textarea."
-          />
-        </div>
-        <div>
-          <button onClick={() => this.copyCodeToClipboard()}>
-            Copy to Clipboard
-          </button>
-          {this.state.copySuccess ? (
-            <div style={{ color: "green" }}>Success!</div>
-          ) : null}
-        </div>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <>
+      <Card>
+        <Reguler />
+      </Card>
+      <Card>
+        <AsyncForm />
+      </Card>
+    </>
+  );
 }
+
+const Card = ({ children }) => (
+  <Stack p={"20px"} borderRadius={"20px"} bg="tersier.main" mt="20px">
+    {children}
+  </Stack>
+);
+
+export const Header = ({
+  dirtyFields,
+  errors,
+  isDirty,
+  isSubmitSuccessful,
+  isSubmitted,
+  isSubmitting,
+  isValid,
+  isValidating,
+  submitCount,
+  touchedFields,
+  title,
+}) => (
+  <>
+    <Text fontSize={"24px"} color={"black"} textTransform={"uppercase"}>
+      {title}
+    </Text>
+    <Stack direction={"row"} color={"white"} fontWeight={"bold"}>
+      <Text>
+        Is Dirty : <span style={{ color: "black" }}>{`${isDirty}`} </span>|
+      </Text>
+      <Text>
+        isSubmitSuccessful :
+        <span style={{ color: "black" }}>{`${isSubmitSuccessful}`} </span>|
+      </Text>
+      <Text>
+        isSubmitted :<span style={{ color: "black" }}>{`${isSubmitted}`} </span>
+        |
+      </Text>
+      <Text>
+        isSubmitting :
+        <span style={{ color: "black" }}>{`${isSubmitting}`} </span>|
+      </Text>
+      <Text>
+        isValid :<span style={{ color: "black" }}>{`${isValid}`} </span>|
+      </Text>
+      <Text>
+        isValidating :
+        <span style={{ color: "black" }}>{`${isValidating}`} </span>|
+      </Text>
+    </Stack>
+    <Stack direction={"row"} color={"white"} fontWeight={"bold"}>
+      <Text>
+        submitCount :<span style={{ color: "black" }}>{`${submitCount}`} </span>
+        |
+      </Text>
+      <Text>
+        errors :
+        <span style={{ color: "black" }}>{`${JSON.stringify(errors)}`} </span>|
+      </Text>
+      <Text>
+        touchedFields :
+        <span style={{ color: "black" }}>
+          {`${JSON.stringify(touchedFields)}`}{" "}
+        </span>
+        |
+      </Text>
+    </Stack>
+    <Stack direction={"row"} color={"white"} fontWeight={"bold"}>
+      <Text>
+        dirtyFields :
+        <span style={{ color: "black" }}>
+          {`${JSON.stringify(dirtyFields)}`}
+        </span>
+        |
+      </Text>
+    </Stack>
+  </>
+);
